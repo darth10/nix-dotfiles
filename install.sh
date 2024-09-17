a0de59dde987ae2b6c9312eb5aa394cebc0a1f1c
Akhil Wali <akhil.wali.10@gmail.com>
2024-09-18 10:39:29 +1200

    Link to `/etc/nixos` after backup

--- install.sh
+++ install.sh
@@ -12,10 +12,8 @@ if [ -e /etc/NIXOS ]; then
     # Make sure changes to `hardware-configuration.nix` are staged or committed:
     nixos-generate-config --show-hardware-config > $PWD/modules/nixos/hardware-configuration.nix
 
-    sudo rm /etc/nixos/configuration.nix
-    sudo ln -s $PWD/modules/nixos/configuration.nix /etc/nixos/configuration.nix
-    sudo ln -s $PWD/modules/nixos/flake.nix /etc/nixos/flake.nix
-    sudo ln -s $PWD/modules/nixos/flake.lock /etc/nixos/flake.lock
+    sudo mv /etc/nixos /etc/nixos.orig
+    sudo ln -s $PWD/modules/nixos /etc/nixos
 fi
 
 nix run home-manager/release-24.05 -- init --impure --switch $PWD/modules/home-manager
