28184ec6f6e69c12bed343b26a6c908953b68b65
Akhil Wali <akhil.wali.10@gmail.com>
2024-09-18 01:43:10 +1200

    Remove --impure from nixos-rebuild build

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ ln -s $PWD/modules/home-manager $HOME/.config/home-manager
 ln -s $PWD/modules/doom $HOME/.config/doom
 
 if [ -e /etc/NIXOS ]; then
+    # Make sure changes to `hardware-configuration.nix` are staged or committed:
+    nixos-generate-config --show-hardware-config > $PWD/modules/nixos/hardware-configuration.nix
+
     sudo rm /etc/nixos/configuration.nix
     sudo ln -s $PWD/modules/nixos/configuration.nix /etc/nixos/configuration.nix
     sudo ln -s $PWD/modules/nixos/flake.nix /etc/nixos/flake.nix
--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -9,7 +9,7 @@
 
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
+      ./hardware-configuration.nix
     ];
 
   # Bootloader.
