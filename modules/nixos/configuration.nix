d4cd98e123ace422903317895b8eca7fd04cc090
Akhil Wali <akhil.wali.10@gmail.com>
2024-09-18 21:59:40 +1200

    Add some Gnome extensions

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -116,6 +116,8 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     tailscale
+    gnomeExtensions.hue-lights
+    gnomeExtensions.unite
   ];
 
   fonts.packages = with pkgs; [
