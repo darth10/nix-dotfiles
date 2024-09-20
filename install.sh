9833248ddfeb897b932c1296cf7778eed63c2c9f
Akhil Wali <akhil.wali.10@gmail.com>
2024-09-20 12:51:35 +1200

    Add info for non-VM host to install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,24 @@ set -xeuo pipefail
 
 PWD=$(pwd)
 
+# For README:
+# nix profile install nixpkgs#git --extra-experimental-features 'nix-command flakes' --priority 7
+# git clone ~/Downloads/dotfiles-nix.bundle ~/.nix-dotfiles -b master --recursive
+
 ln -s $PWD $HOME/.config/home-manager
 ln -s $PWD/modules/nix $HOME/.config/nix # TODO remove this module
 ln -s $PWD/modules/doom $HOME/.config/doom
 
+pushd $HOME/.config/home-manager
+nix run --extra-experimental-features 'nix-command flakes' home-manager/release-24.05 -- init --switch .
+popd
+
 if [ -e /etc/NIXOS ]; then
     # Make sure changes to `hardware-configuration.nix` are staged or committed:
-    nixos-generate-config --show-hardware-config > $PWD/modules/nixos/hardware-configuration.nix
+    cp /etc/nixos/hardware-configuration.nix $PWD/modules/nixos/hardware-configuration.nix
 
     sudo mv /etc/nixos /etc/nixos.orig
     sudo ln -s $PWD /etc/nixos
+    sudo nixos-rebuild switch
+    sudo nixos-rebuild switch --flake $PWD#starf0rge
 fi
-
-nix run home-manager/release-24.05 -- init --switch $PWD/modules/home-manager
