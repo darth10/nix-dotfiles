e661971d13ba6db0f01b52f7fcb423e2b523c585
Akhil Wali <akhil.wali.10@gmail.com>
2024-09-21 00:48:45 +1200

    Add git config

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -147,8 +147,30 @@
   # TODO use .gitconfig from dotfiles, move to .config/git/config
   programs.git = {
     enable = true;
+
     userName = "Akhil Wali";
     userEmail = "akhil.wali.10@gmail.com";
+
+    extraConfig = {
+      commit.gpgsign = true;
+      user.signingkey = "CBA0458B682A8544";
+
+      github.user = "darth10";
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
+    };
+
+    delta = {
+      enable = true;
+      options = {
+        feature = "theme";
+        navigate = true;
+        theme = {
+          dark = true;
+          syntax-theme = "Nord";
+        };
+      };
+    };
   };
 
   # TODO use .zshrm from dotfiles
