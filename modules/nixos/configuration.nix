31eb12502b011ba7c3c7442468415f4b16f79e20
Akhil Wali <akhil.wali.10@gmail.com>
2024-09-21 22:37:41 +1200

    Add dnsutils

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -123,7 +123,9 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    dig.dnsutils
     tailscale
+
     gnomeExtensions.hue-lights
     gnomeExtensions.unite
   ];
