e1989c2a8d3b0bbca5b37052f85cc1e09f7b9290
Akhil Wali <akhil.wali.10@gmail.com>
2024-09-23 23:07:47 +1200

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -16,43 +16,49 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs_22_11, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      lib = nixpkgs.lib // home-manager.lib;
 
-      pkgs_22_11 = import nixpkgs_22_11 {
+      pkgs_22_11 = import inputs.nixpkgs_22_11 {
         inherit system;
         config.allowUnfreePredicate = pkg:
-          builtins.elem (nixpkgs.lib.getName pkg) (map nixpkgs.lib.getName [
+          builtins.elem (lib.getName pkg) (map lib.getName [
             pkgs_22_11.pcloud
           ]);
       };
     in {
-      nixosConfigurations."starf0rge" = nixpkgs.lib.nixosSystem {
-        inherit system;
+      nixosConfigurations = {
+        "starf0rge" = lib.nixosSystem {
+          inherit system;
 
-        specialArgs = { inherit inputs; inherit pkgs_22_11; };
-        modules = [
-          ./modules/nixos/configuration.nix
-          ({ pkgs, ... }: {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./modules/nixos/configuration.nix
+            ({ ... }: {
               environment.systemPackages = [
-                self.packages.${pkgs.stdenv.system}.pcloud
+                self.packages.x86_64-linux.pcloud
               ];
-          })
-        ];
+            })
+          ];
+        };
       };
 
-      homeConfigurations."darth10" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      homeConfigurations = {
+        "darth10@starf0rge" = lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = { inherit inputs; };
 
-        modules = [
-          ./modules/home-manager/home.nix
-          inputs.nix-index-database.hmModules.nix-index
-        ];
+          modules = [
+            ./modules/home-manager/home.nix
+            inputs.nix-index-database.hmModules.nix-index
+          ];
+        };
       };
 
-      packages.${system}.pcloud = pkgs_22_11.pcloud.overrideAttrs (prev:
+      packages.x86_64-linux.pcloud = pkgs_22_11.pcloud.overrideAttrs (prev:
         let
           version = "1.14.7";
           code = "XZhPkU0Zh5gulxHfMn4j1dYBS4dh45iDQHby";
