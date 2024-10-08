46560fb3141d0ca65771a9c4a470f1aa2ce6e85a
Akhil Wali <akhil.wali.10@gmail.com>
2024-10-09 10:39:10 +1300

    i18n.nix

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -13,6 +13,7 @@
     ./fonts.nix
     ./keyd.nix
     ./virtualisation.nix
+    ./i18n.nix
     ./nodejs.nix
   ];
 
@@ -32,23 +33,6 @@
   networking = {
     hostName = "starf0rge";
     networkmanager.enable = true;
-  };
-
-  # TODO move to i18n.nix
-  i18n = {
-    defaultLocale = "en_NZ.UTF-8";
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_NZ.UTF-8";
-      LC_IDENTIFICATION = "en_NZ.UTF-8";
-      LC_MEASUREMENT = "en_NZ.UTF-8";
-      LC_MONETARY = "en_NZ.UTF-8";
-      LC_NAME = "en_NZ.UTF-8";
-      LC_NUMERIC = "en_NZ.UTF-8";
-      LC_PAPER = "en_NZ.UTF-8";
-      LC_TELEPHONE = "en_NZ.UTF-8";
-      LC_TIME = "en_NZ.UTF-8";
-    };
   };
 
   users.users.darth10 = {
