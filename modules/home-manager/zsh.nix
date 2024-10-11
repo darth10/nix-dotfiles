7c37b76b8e4b0a50fc34be54b926e6c6cf6bb3c8
Akhil Wali <akhil.wali.10@gmail.com>
2024-10-12 00:20:04 +1300

    Configure ZVM prompt and cursor

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -47,7 +47,14 @@
 
       initExtra = ''
         ZVM_INIT_MODE=sourcing
+
         source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+
+        ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BEAM
+        ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
+        ZVM_VISUAL_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
+        ZVM_VISUAL_LINE_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
+        ZVM_OPPEND_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
       '';
 
       oh-my-zsh = {
