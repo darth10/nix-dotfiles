b13ade8c0aab33f46c6f512733b97269cf13f906
Akhil Wali <akhil.wali.10@gmail.com>
2024-10-18 00:57:16 +1300

    Fix nix warnings

--- lib/nix.settings.nix
+++ lib/nix.settings.nix
@@ -4,6 +4,7 @@
       experimental-features = ["nix-command" "flakes"];
       use-xdg-base-directories = true;
       auto-optimise-store = true;
+      trusted-users = ["@wheel"];
     };
   };
 }
