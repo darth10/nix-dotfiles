73dfa3b1b86d2061bffc5666e0cc66d44750f197
Akhil Wali <akhil.wali.10@gmail.com>
2024-10-18 01:17:58 +1300

    Add monitor config for GDM

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -6,6 +6,10 @@
     gnomeExtensions.another-window-session-manager
   ];
 
+  systemd.tmpfiles.rules = [
+    ''f+ /run/gdm/.config/monitors.xml - gdm gdm - <monitors version="2"><configuration><logicalmonitor><x>0</x><y>0</y><scale>1</scale><primary>yes</primary><monitor><monitorspec><connector>VGA-1</connector><vendor>BNQ</vendor><product>BenQ BL2480T</product><serial>31N0698801Q</serial></monitorspec><mode><width>1920</width><height>1080</height><rate>60.000</rate></mode></monitor></logicalmonitor><disabled><monitorspec><connector>LVDS-1</connector><vendor>LGD</vendor><product>0x03b1</product><serial>0x00000000</serial></monitorspec></disabled></configuration></monitors>''
+  ];
+
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
