33cb62e7371edcaeb6a74abb2b94834ac3eb59c8
Akhil Wali <akhil.wali.10@gmail.com>
2024-10-23 16:01:45 +1300

    Move virtiofsd

--- modules/nixos/virtualisation.nix
+++ modules/nixos/virtualisation.nix
@@ -1,11 +1,17 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     docker-compose
-    virtiofsd
   ];
 
-  virtualisation.docker.enable = true;
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    docker.enable = true;
+
+    libvirtd = {
+      enable = true;
+      qemu.vhostUserPackages = [pkgs.virtiofsd];
+    };
+  };
+
   programs.virt-manager.enable = true;
 
   users.users.darth10.extraGroups = ["docker" "libvirtd"];
