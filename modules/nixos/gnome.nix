b828c041cf6769cc497ce3421e79ca92ef5cea63
Akhil Wali <akhil.wali.10@gmail.com>
2025-05-13 00:04:18 +1200

    Update monitor config for GDM

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -10,7 +10,101 @@
   ];
 
   systemd.tmpfiles.rules = [
-    ''f+ /run/gdm/.config/monitors.xml - gdm gdm - <monitors version="2"><configuration><logicalmonitor><x>0</x><y>0</y><scale>1</scale><primary>yes</primary><monitor><monitorspec><connector>VGA-1</connector><vendor>BNQ</vendor><product>BenQ BL2480T</product><serial>31N0698801Q</serial></monitorspec><mode><width>1920</width><height>1080</height><rate>60.000</rate></mode></monitor></logicalmonitor><disabled><monitorspec><connector>LVDS-1</connector><vendor>LGD</vendor><product>0x03b1</product><serial>0x00000000</serial></monitorspec></disabled></configuration></monitors>''
+      # This should all be copied from ~/.config/monitors.xml:
+    "L+ /run/gdm/.config/monitors.xml - - - - ${pkgs.writeText "gdm-monitors.xml" ''
+      <monitors version="2">
+        <configuration>
+          <layoutmode>logical</layoutmode>
+          <logicalmonitor>
+            <x>0</x>
+            <y>0</y>
+            <scale>1</scale>
+            <primary>yes</primary>
+            <monitor>
+              <monitorspec>
+                <connector>VGA-1</connector>
+                <vendor>BNQ</vendor>
+                <product>BenQ BL2480T</product>
+                <serial>31N0698801Q</serial>
+              </monitorspec>
+              <mode>
+                <width>1920</width>
+                <height>1080</height>
+                <rate>60.000</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+          <disabled>
+            <monitorspec>
+              <connector>LVDS-1</connector>
+              <vendor>LGD</vendor>
+              <product>0x03b1</product>
+              <serial>0x00000000</serial>
+            </monitorspec>
+          </disabled>
+        </configuration>
+        <configuration>
+          <layoutmode>physical</layoutmode>
+          <logicalmonitor>
+            <x>0</x>
+            <y>0</y>
+            <scale>1</scale>
+            <primary>yes</primary>
+            <monitor>
+              <monitorspec>
+                <connector>VGA-1</connector>
+                <vendor>BNQ</vendor>
+                <product>BenQ BL2480T</product>
+                <serial>31N0698801Q</serial>
+              </monitorspec>
+              <mode>
+                <width>1920</width>
+                <height>1080</height>
+                <rate>60.000</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+          <disabled>
+            <monitorspec>
+              <connector>LVDS-1</connector>
+              <vendor>LGD</vendor>
+              <product>0x03b1</product>
+              <serial>0x00000000</serial>
+            </monitorspec>
+          </disabled>
+        </configuration>
+        <configuration>
+          <layoutmode>physical</layoutmode>
+          <logicalmonitor>
+            <x>0</x>
+            <y>0</y>
+            <scale>1</scale>
+            <primary>yes</primary>
+            <monitor>
+              <monitorspec>
+                <connector>VGA-1</connector>
+                <vendor>BNQ</vendor>
+                <product>BenQ BL2480T</product>
+                <serial>31N0698801Q</serial>
+              </monitorspec>
+              <mode>
+                <width>1920</width>
+                <height>1080</height>
+                <rate>60.000</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+          <disabled>
+            <monitorspec>
+              <connector>LVDS-1</connector>
+              <vendor>LGD</vendor>
+              <product>0x03a3</product>
+              <serial>0x00000000</serial>
+            </monitorspec>
+          </disabled>
+        </configuration>
+      </monitors>
+    ''}"
   ];
 
   services.xserver = {
