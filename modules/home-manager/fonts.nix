d8867f5f967f5d989bed1096b585ffa1df05b669
Akhil Wali <akhil.wali.10@gmail.com>
2026-03-06 23:00:44 +1300

    Add consolas-ligaturized font package

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -1,7 +1,35 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  consolas-ligaturized = pkgs.stdenvNoCC.mkDerivation {
+    pname = "consolas-ligaturized";
+    version = "3";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "somq";
+      repo = "consolas-ligaturized";
+      rev = "d11d670146a58b5a1ffd9f6a44cef432e1678e1e";
+      hash = "sha256-uJCByvY1Aq1S+aL7dcoxDqMMWHMT4V8dJT/KdOaN9Nk=";
+    };
+
+    installPhase = ''
+      install -Dm644 *.ttf -t $out/share/fonts/truetype/consolas-ligaturized
+    '';
+
+    meta = {
+      description = "Consolas font with FiraCode ligatures";
+      homepage = "https://github.com/somq/consolas-ligaturized";
+      license = lib.licenses.ofl;
+      platforms = lib.platforms.all;
+    };
+  };
+in {
   home.packages = with pkgs; [
     fontconfig
     nerd-fonts.fira-code
+    consolas-ligaturized
   ];
 
   fonts.fontconfig.enable = true;
