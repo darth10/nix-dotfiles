dda6679cc8bdc3157539b37317404f253f7aa3b0
Akhil Wali <akhil.wali.10@gmail.com>
2026-09-10 23:15:03 +1200

    Fix install script

--- install.sh
+++ install.sh
@@ -3,21 +3,21 @@
 # Add -x flag to debug
 set -euo pipefail
 
-nix run home-manager/release-24.05 -- -b backup switch \
+nix run github:nix-community/home-manager -- -b backup switch \
     --flake ".#$USER"
 
 # NixOS
 if [ -e /etc/NIXOS ]; then
-    PWD=$(pwd)
+    REPO_DIR=$(pwd)
     # Make sure changes to `hardware-configuration.nix` are staged or committed:
-    cp /etc/nixos/hardware-configuration.nix "$PWD/modules/hosts/$HOST/hardware-configuration.nix"
+    cp /etc/nixos/hardware-configuration.nix "$REPO_DIR/modules/hosts/$HOST/hardware-configuration.nix"
     sudo mv /etc/nixos /etc/nixos.orig
-    sudo ln -s $PWD /etc/nixos
+    sudo ln -s $REPO_DIR /etc/nixos
     sudo nixos-rebuild switch --flake ".#$HOST"
 fi
 
 # Other Linux
-if [ ! -e /etc/NIXOS ] && [ $OSTYPE == 'linux-gnu' ]; then
+if [[ ! -e /etc/NIXOS && $OSTYPE == 'linux-gnu' ]]; then
      NIX_ZSH=$(ls ~/.local/state/nix/profile/bin/zsh)
      printf "\n\e[93mAdding %s to /etc/shells\e[0m\n\n" $NIX_ZSH
      echo $NIX_ZSH | sudo tee -a /etc/shells > /dev/null
